54cb28cf0c08293331aa2ff1014c6d3af0d02b71
Felipe Gil <felipe.gil@redspace.com>
2021-05-31 17:18:06 -0300

    First Commit

---
+++ ImagesWithFileManager/DetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.swift
+//  ImagesWithFileManager
+//
+//  Created by Felipe Gil on 2021-05-25.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    var nameOfImage: String?
+    var numberOfImage: Int = 0
+    var numberOfImages: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(numberOfImage) of \(numberOfImages)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = nameOfImage {
+            imgView.image = UIImage (named: imageToLoad)
+        }
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
